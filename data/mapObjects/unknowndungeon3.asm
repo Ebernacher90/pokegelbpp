c86a922db86227708e16df4ae8bacbf3fc7b40a2
Ebernacher90 <danielvonderbank@gmail.com>
2020-03-27 22:30:11 +0100

    Restore to Pokegelb++ newest version

--- data/mapObjects/unknowndungeon3.asm
+++ data/mapObjects/unknowndungeon3.asm
@@ -7,7 +7,7 @@ UnknownDungeon3Object:
 	db 0 ; signs
 
 	db 5 ; objects
-	object SPRITE_SLOWBRO, 27, 13, STAY, DOWN, 1, MEWTWO, 70
+	object SPRITE_SLOWBRO, 27, 13, STAY, DOWN, 1, MEWTWO, 2
 	object SPRITE_BALL, 26, 1, STAY, NONE, 2, ULTRA_BALL
 	object SPRITE_BALL, 2, 13, STAY, NONE, 3, ULTRA_BALL
 	object SPRITE_BALL, 3, 13, STAY, NONE, 4, MAX_REVIVE
