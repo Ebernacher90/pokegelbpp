3679da155c3fd3b91b0dfd0240d37473a1cb36a5
Ebernacher90 <danielvonderbank@gmail.com>
2020-06-08 12:46:38 +0200

    Fixed Elevoltek Evolutions Error to Rasaff

--- data/evos_moves.asm
+++ data/evos_moves.asm
@@ -847,13 +847,13 @@ KoffingEvosMoves:
 
 ElevoltekEvosMoves:
 ; Evolutions
+	db 0
+; Learnset
 	db 34, THUNDERSHOCK
 	db 37, SCREECH
 	db 42, THUNDERPUNCH
 	db 49, LIGHT_SCREEN
 	db 54, THUNDER
-	db 0
-; Learnset
 	db 0
 
 MankeyEvosMoves:
