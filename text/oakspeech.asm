c9a6d80b9861b88be8189a0fda9eb88811e76f70
Ebernacher90 <danielvonderbank@gmail.com>
2020-06-18 22:27:26 +0200

    Fixed Oakspeech

--- text/oakspeech.asm
+++ text/oakspeech.asm
@@ -14,7 +14,7 @@ _OakSpeechText1::
 	cont "verbesserte"
 	cont "Inhalte hat. Dies"
 	cont "ist Version:"
-	cont "0.1.7.3!"
+	cont "0.1.7.3.2.1!"
 	cont "dies ist die"
 	cont "allerneuste"
 	cont "Version, viel"
